01f018dcf72c057295ba2ba2ebaff3fbace84c1a
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-19 10:57:09 -0700

    budget db

--- public/db.js
+++ public/db.js
@@ -1,7 +1,9 @@
+const { response } = require("express");
+
 let db;
 let budgetVersion;
 
-const request = indexDB.open('budgetdb' budgetVersion || 21);
+const request = indexDB.open('budgetdb', budgetVersion || 21);
 
 request.onupgradeneeded = function (e) {
     console.log('upgrade needed');
@@ -17,3 +19,38 @@ request.onupgradeneeded = function (e) {
         db.createObjectStore('BudgetStore', {autoIncrement: true });
     }
 };
+
+request.onerror = function (e) {
+    console.log(`Woops! ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+    let transaction = db.transaction(['BudgetStore'],
+    'readwrite');
+
+    const store = transaction.objectStore('BudgetStore');
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result. length > 0) {
+            fetch('api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*', 'Content-type': 'application/json'
+                },
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    transaction = db.transaction(["BudgetStore"], 'readwrite');
+
+                    const currentStore = transaction.objectStore('BudgetStore');
+
+                    currentStore.clear();
+                }
+            });
+        }
+    }
+}
